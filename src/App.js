512af4fadb9803e62099c3d121cd8005f84a29db
Kim Murray <kimberley.murray@hotmail.co.uk>
2021-12-09 19:00:26 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./styles/App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Dictionary</h1>
+      <button type="button" className="btn btn-primary">
+        Bootstrap Test
+      </button>
+    </div>
+  );
+}
