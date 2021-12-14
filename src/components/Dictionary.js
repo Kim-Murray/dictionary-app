76b818a17bec86e3464cce0bd0fbe041030876b9
Kim Murray <kimberley.murray@hotmail.co.uk>
2021-12-14 12:53:24 +0000

    Installed axios and linked dictionary API
    https://dictionaryapi.dev/

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import "../styles/Dictionary.css";
 
 export default function Dictionary() {
   const [word, setWord] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function searchWord(event) {
     event.preventDefault();
     alert(`Searching for the word ${word}.`);
+
+    let dictionaryURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+    axios.get(dictionaryURL).then(handleResponse);
   }
 
   function updateWord(event) {
