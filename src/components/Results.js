605981c50c56172f0c5853ceb91b6e005eb88a9d
Kim Murray <kimberley.murray@hotmail.co.uk>
2022-01-05 17:57:11 +0000

    Added photo as placeholder for photos relating to the words

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,6 +2,8 @@ import React from "react";
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
 
+import img from "../images/dictionaryCropped.jpg";
+
 export default function Results(props) {
   console.log(props.results);
   if (props.results) {
@@ -14,16 +16,26 @@ export default function Results(props) {
 
     return (
       <div className="results">
-        <section>
-          <h2>{results.word}</h2>
-          {props.results[0].phonetics.map(function (phonetics, index) {
-            return (
-              <div key={index}>
-                <Phonetic phonetics={phonetics} />
-              </div>
-            );
-          })}
-        </section>
+        <div className="row">
+          <div className="col-4">
+            <section>
+              <h2>{results.word}</h2>
+              {props.results[0].phonetics.map(function (phonetics, index) {
+                return (
+                  <div key={index}>
+                    <Phonetic phonetics={phonetics} />
+                  </div>
+                );
+              })}
+            </section>
+          </div>
+
+          <div className="col-8">
+            <section>
+              <img src={img} alt="image of word" className="img-fluid" />
+            </section>
+          </div>
+        </div>
         {props.results[0].meanings.map(function (meaning, index) {
           return (
             <div key={index}>
